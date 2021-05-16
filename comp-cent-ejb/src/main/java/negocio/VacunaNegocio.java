3d73226401f8a1f5fefebb8ecf758f11bd71867d
Hernan <hernanbascuade@gmail.com>
2021-05-15 21:10:47 -0300

    Agregando Clases

---
+++ comp-cent-ejb/src/main/java/negocio/VacunaNegocio.java
@@ -0,0 +1,67 @@
+package negocio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+
+import datatypes.DTVacuna;
+import datos.VacunaDato;
+import entidades.Enfermedad;
+import entidades.Proveedor;
+import entidades.Vacuna;
+
+/**
+ * Session Bean implementation class vacunaNegocio
+ */
+@Stateless
+@LocalBean
+public class VacunaNegocio implements VacunaNegocioRemote, VacunaNegocioLocal {
+
+	@SuppressWarnings("unused")
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private VacunaDato puenteDatos;
+	
+    /**
+     * Default constructor. 
+     */
+    public VacunaNegocio() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void agregarVacuna(String nombre, String codigo, String laboratorio, Enfermedad enf, Proveedor pro) {
+    	
+    	Vacuna vac= new Vacuna();
+    	vac.setNombre(nombre);
+    	vac.setCodigo(codigo);
+    	vac.setLaboratorio(laboratorio);
+    	
+    	
+    	puenteDatos.agregarVacuna(vac);
+    	
+    }
+    
+    public DTVacuna obtenerVacuna(long id) {
+	    	Vacuna vac = puenteDatos.obtenerVacuna(id);
+	    	return new DTVacuna(vac.getNombre(), vac.getCodigo(), vac.getLaboratorio());
+    }
+    
+    public List<DTVacuna> obtenerVacunas(){
+    	
+    	ArrayList<Vacuna> vacs = (ArrayList<Vacuna>)puenteDatos.obtenerVacunas();
+    	ArrayList<DTVacuna> dtVacs =  new ArrayList<DTVacuna>();
+    	for(Vacuna vac : vacs)
+    	{
+    		dtVacs.add(new DTVacuna(vac.getNombre(), vac.getCodigo(), vac.getLaboratorio()));
+    	}
+    	return dtVacs;
+    }
+    public void agregarVacunas() {
+    	puenteDatos.agregarVacunas();
+    }
+}
