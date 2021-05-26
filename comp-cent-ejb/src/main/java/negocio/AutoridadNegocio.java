afe35a7524c5388dc29a66a2fec190363bf823e5
Antony <antonyh891@gmail.com>
2021-05-26 01:27:10 -0300

    correccion AutoridadNegocio

--- comp-cent-ejb/src/main/java/negocio/AutoridadNegocio.java
+++ comp-cent-ejb/src/main/java/negocio/AutoridadNegocio.java
@@ -1,7 +1,12 @@
 package negocio;
 
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import datatypes.DTAutoridad;
+import datos.AutoridadDatoLocal;
+import entidades.Autoridad;
 
 /**
  * Session Bean implementation class AutoridadNegocio
@@ -10,11 +15,13 @@ import javax.ejb.Stateless;
 @LocalBean
 public class AutoridadNegocio implements AutoridadNegocioRemote, AutoridadNegocioLocal {
 
-    /**
-     * Default constructor. 
-     */
+	@EJB
+	private AutoridadDatoLocal datoLocal ;
+	
     public AutoridadNegocio() {
         // TODO Auto-generated constructor stub
     }
+    
+    
 
 }
