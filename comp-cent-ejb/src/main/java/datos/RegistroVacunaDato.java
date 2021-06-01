f8d7990e11cc4d108580e7389add4f85550fe99a
Esteban Petrone <luis.petrone@fing.edu.uy>
2021-06-01 01:54:22 -0300

    Rest para certificado de vacunacion

---
+++ comp-cent-ejb/src/main/java/datos/RegistroVacunaDato.java
@@ -0,0 +1,46 @@
+package datos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import entidades.RegistroVacuna;
+
+/**
+ * Session Bean implementation class RegistroVacunaDato
+ */
+@Stateless
+@LocalBean
+public class RegistroVacunaDato implements RegistroVacunaDatoLocal {
+
+	@PersistenceContext(name = "comp-centPersistenceUnit")
+	private EntityManager em;
+    /**
+     * Default constructor. 
+     */
+    public RegistroVacunaDato() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void agregarRegistroVacuna(RegistroVacuna regVac) {
+		em.persist(regVac);
+	}
+    
+    @Override
+	public List<RegistroVacuna> obtenerRegistroPorCi (int ci) {
+    	
+		List<RegistroVacuna> listRegVac = new ArrayList<RegistroVacuna>();
+		for	(RegistroVacuna rv :em.createQuery("Select rv from RegistroVacuna rv where rv.ciudadano.ci = :ci",RegistroVacuna.class).setParameter("ci", ci).getResultList()) {
+			listRegVac.add(rv);
+		}
+		return listRegVac;	
+		    	
+	}
+    
+    
+}
