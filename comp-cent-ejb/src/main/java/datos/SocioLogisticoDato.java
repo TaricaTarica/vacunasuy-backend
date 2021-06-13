d32320df45c58b8f4b5e5603327aa058b486544b
Esteban Petrone <luis.petrone@fing.edu.uy>
2021-06-12 21:26:41 -0300

    Socio Logistico DataLayer and Business Layer

--- comp-cent-ejb/src/main/java/datos/SocioLogisticoDato.java
+++ comp-cent-ejb/src/main/java/datos/SocioLogisticoDato.java
@@ -2,6 +2,10 @@ package datos;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import entidades.SocioLogistico;
 
 /**
  * Session Bean implementation class SocioLogisticoDato
@@ -10,11 +14,46 @@ import javax.ejb.Stateless;
 @LocalBean
 public class SocioLogisticoDato implements SocioLogisticoDatoLocal {
 
+	@PersistenceContext(name = "comp-centPersistenceUnit")
+	private EntityManager em;
     /**
      * Default constructor. 
      */
     public SocioLogisticoDato() {
         // TODO Auto-generated constructor stub
     }
+    
+    @Override
+	public void agregarSocioLogistico(SocioLogistico socio) {
+    	em.persist(socio);
+	}
+	
+    @Override
+	public void editarSocioLogistico(SocioLogistico socio) {
+		em.merge(socio);
+	}
+	
+    @Override
+	public void eliminarSocioLosgistico(SocioLogistico socio) {
+		em.remove(socio);
+	}
+	
+    @Override
+	public Boolean existeSocioLogistico(String codigo) {
+		return em.createQuery("select sl from SocioLogistico sl where sl.codigo =:codigo")
+				.setParameter("codigo", codigo).getResultList().size()>0;
+	}
+	
+    @Override
+	public SocioLogistico obtenerSocioLogistico(String codigo) {
+		return (SocioLogistico) em.createNativeQuery("select sl from SocioLogistico sl where sl.codigo =:codigo", SocioLogistico.class)
+				.setParameter("codigo", codigo).getSingleResult();
+	}
 
+    @Override
+	public SocioLogistico obtenerSocioLogisticoPorId(long id) {
+		return (SocioLogistico) em.createNativeQuery("select sl from SocioLogistico sl where sl.id =:id", SocioLogistico.class)
+				.setParameter("id", id).getSingleResult();
+	}
+    
 }
---
+++ comp-cent-ejb/src/main/java/negocio/SocioLogisticoNegocioLocal.java
@@ -0,0 +1,20 @@
+package negocio;
+
+import javax.ejb.Local;
+
+import datatypes.DTSocioLogistico;
+
+@Local
+public interface SocioLogisticoNegocioLocal {
+
+	public void agregarSocioLogistico(DTSocioLogistico dtSocio) throws Exception;
+	
+	public void editarSocioLogistico(DTSocioLogistico dtSocio) throws Exception;
+	
+	public void eliminarSocioLosgistico(DTSocioLogistico dtSocio) throws Exception;
+	
+	public Boolean existeSocioLogistico(String codigo);
+	
+	public DTSocioLogistico obtenerSocioLogistico(String codigo) throws Exception;
+	
+}
