e5582158e67990deb9009fe13658308ac4a78098
Hernan <hernanbascuade@gmail.com>
2021-06-29 14:01:48 -0300

    Ubicacion a los vacunatorios GoogleMaps

--- comp-cent-web/src/main/java/beans/GoogleMapsBean.java
+++ comp-cent-web/src/main/java/beans/GoogleMapsBean.java
@@ -1,13 +1,16 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import datatypes.DTVacunatorio;
 import negocio.VacunatorioGeomNegocioLocal;
+import negocio.VacunatorioNegocioLocal;
 
 @Named("googleMapsBean")
 @ViewScoped
@@ -19,12 +22,38 @@ public class GoogleMapsBean implements Serializable {
 	
 	@EJB
 	private VacunatorioGeomNegocioLocal geomLocal;
+	@EJB
+	private VacunatorioNegocioLocal vacunatorioLocal;
 	
 	private String y;
 	private String x;
 	private String coordenadas;
 	
+	private DTVacunatorio vacunatorio;
+	private List <DTVacunatorio> vacunatorios;
+	private String nombreVacunatorio;
 	
+	
+	
+
+
+	@PostConstruct
+	public void init() {
+		this.vacunatorios = vacunatorioLocal.listarVacunatorio();
+		
+	} 
+	
+	
+	public String getNombreVacunatorio() {
+		return nombreVacunatorio;
+	}
+
+
+	public void setNombreVacunatorio(String nombreVacunatorio) {
+		this.nombreVacunatorio = nombreVacunatorio;
+	}
+
+
 	public String getCoordenadas() {
 		return coordenadas;
 	}
@@ -38,21 +67,36 @@ public class GoogleMapsBean implements Serializable {
 
 
 
-
-	@PostConstruct
-	public void init() {
-		
-
-	} 
 	
 	public void publicar() {
-		System.out.println("longitud: " + x);
-		System.out.println("Latitud: " + y);
-		System.out.println("coordenadas: " + coordenadas);
+		for (DTVacunatorio vac : vacunatorios) {
+		
+		if (vac.getNombre().equals(nombreVacunatorio)) {
+			setVacunatorio(vac);
+			System.out.println("Estoy probando " + vacunatorio);
+		} else {
+			System.out.println("Vacunatorio null");
+		}
+		}
+		
 	}
 
 	public void insertarCoordenadas() {
-		geomLocal.agregarCoordenadas(1001, y, x);
+		
+		for (DTVacunatorio vac : vacunatorios) {
+			
+			if (vac.getNombre().equals(nombreVacunatorio)) {
+				setVacunatorio(vac);
+				System.out.println(y);
+				System.out.println(x);
+				System.out.println(vacunatorio);
+				System.out.println(vacunatorio.getId());
+				geomLocal.agregarCoordenadas(vacunatorio.getId(), y, x);
+			} else {
+				System.out.println("Vacunatorio null");
+			}
+		}
+		
 	}
 
 
@@ -84,6 +128,35 @@ public class GoogleMapsBean implements Serializable {
 
 
 
+	public DTVacunatorio getVacunatorio() {
+		return vacunatorio;
+	}
 
+
+
+
+	public void setVacunatorio(DTVacunatorio vacunatorio) {
+		this.vacunatorio = vacunatorio;
+	}
+
+
+
+	public void publicarVacunatorio(DTVacunatorio vac) {
+		System.out.println(vac.getId());
+		
+	}
+
+
+	public List <DTVacunatorio> getVacunatorios() {
+		return vacunatorios;
+	}
+
+
+	public void setVacunatorios(List <DTVacunatorio> vacunatorios) {
+		this.vacunatorios = vacunatorios;
+	}
+
+
+	
 	
 }
